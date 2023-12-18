4646cdd776f272bce7ddcecb19b4757898fc0573
K-S-Harish <93591271+K-S-Harish@users.noreply.github.com>
2023-12-18 12:11:57 +0530

    Add files via upload

---
+++ Adding_Individual_Numbers_of_2digitNumb.py
@@ -0,0 +1,12 @@
+#######  Adding individual numbers of a 2-digit Number.  #######
+
+
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+a =  int(two_digit_number[0]) + int(two_digit_number[1])
+# Here we convert the indexed values of the input which we got into INTEGER form, in order to add the together.
+
+print(a)
